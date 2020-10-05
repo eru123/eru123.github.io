2c079ffb928e882639908d4ca8d13419a1d0b125
Eru Roraito <yeoligoakino@email.com>
2020-10-05 22:08:16 +0800

    up

--- js/red-mantis-webparser.js
+++ js/red-mantis-webparser.js
@@ -175,11 +175,7 @@
     return JSON.stringify(htmlToJson(doc));
   }
   async function getFromUrl(url) {
-    return await axios.get(url,{
-       headers: { 
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'
-      }
-    }).then((e) => {
+    return await axios.get(url).then((e) => {
       let text = stripCases(e.data);
 
       return {
